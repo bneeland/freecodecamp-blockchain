ec10898c94a948c34fbd012bce60d5b9aa29446b
Brian Neeland <brian.neeland@protonmail.com>
2021-12-13 17:34:36 -0700

    Transaction going through

--- demos/web3_py_simple_storage/SimpleStorage.sol
+++ demos/web3_py_simple_storage/SimpleStorage.sol
@@ -1,31 +1,30 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity 0.8.10;
+pragma solidity >=0.6.0 <0.9.0;
 
 contract SimpleStorage {
 
-  uint256 favouriteNumber;
-  bool favouriteBool;
+    uint256 favoriteNumber;
 
-  struct People {
-    uint256 favouriteNumber;
-    string name;
-  }
+    // This is a comment!
+    struct People {
+        uint256 favoriteNumber;
+        string name;
+    }
 
-  People[] public people;
-  mapping(string => uint256) public nameToFavouriteNumber;
+    People[] public people;
+    mapping(string => uint256) public nameToFavoriteNumber;
 
-  function store(uint256 _favouriteNumber) public {
-    favouriteNumber = _favouriteNumber;
-  }
+    function store(uint256 _favoriteNumber) public {
+        favoriteNumber = _favoriteNumber;
+    }
 
-  function retrieve() public view returns(uint256) {
-    return favouriteNumber;
-  }
+    function retrieve() public view returns (uint256){
+        return favoriteNumber;
+    }
 
-  function addPerson(string memory _name, uint256 _favouriteNumber) public {
-    people.push(People({favouriteNumber: _favouriteNumber, name: _name}));
-    nameToFavouriteNumber[_name] = _favouriteNumber;
-  }
-
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber, _name));
+        nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
 }
--- demos/web3_py_simple_storage/deploy.py
+++ demos/web3_py_simple_storage/deploy.py
@@ -1,4 +1,6 @@
 from solcx import compile_standard
+import json
+from web3 import Web3
 
 with open('./SimpleStorage.sol', 'r') as file:
     simple_storage_file = file.read()
@@ -20,5 +22,34 @@ compiled_sol = compile_standard(
             }
         }
     },
+    solc_version='0.6.0',
 )
-print(compiled_sol)
+
+with open('compiled_code.json', 'w') as file:
+    json.dump(compiled_sol, file)
+
+# Get bytecode
+bytecode = compiled_sol['contracts']['SimpleStorage.sol']['SimpleStorage']['evm']['bytecode']['object']
+
+# Get ABI
+abi = compiled_sol['contracts']['SimpleStorage.sol']['SimpleStorage']['abi']
+
+# Connect to Ganache
+w3 = Web3(Web3.HTTPProvider("http://0.0.0.0:8545"))
+chain_id = 1337
+my_address = "0x3Fbd234Af26Ce5eF813De9EC0f3495346d24683F"
+private_key = "48736081a5d6f031b25964436619401a437430a9e629ceddfa2b6320d2d3ebaf"
+
+# Create contract
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+# Get the latest transaction count
+nonce = w3.eth.getTransactionCount(my_address)
+transaction = SimpleStorage.constructor().buildTransaction(
+    {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+print(transaction)
