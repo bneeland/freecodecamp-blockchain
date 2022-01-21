5528f2fe5aa540c1cfa599c7830894b6b0db96be
Brian Neeland <brian.neeland@protonmail.com>
2022-01-21 14:42:13 -0700

    Implemented MockV3Aggregator and configs to deploy to development or Rinkeby network

--- demos/brownie_fund_me/scripts/deploy.py
+++ demos/brownie_fund_me/scripts/deploy.py
@@ -1,9 +1,26 @@
-from brownie import FundMe
+from brownie import FundMe, MockV3Aggregator, network, config
 from scripts.helpful_scripts import get_account
 
 def deploy_fund_me():
     account = get_account()
-    fund_me = FundMe.deploy({"from": account}, publish_source=True)
+    # Pass the price feed address to our FundMe contract
+
+    # If we are on a persistent network like Rinkeby, use the associated address,
+    # otherwise, deploy mocks
+    if network.show_active() != "development":
+        price_feed_address = config["networks"][network.show_active()]["eth_usd_price_feed"]
+    else:
+        print(f"The active network is {network.show_active()}")
+        print("Deploying mocks")
+        mock_aggregator = MockV3Aggregator.deploy(18, 2000_000_000_000_000_000_000, {"from": account})
+        price_feed_address = mock_aggregator.address
+        print("Mocks deployed")
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
     print(f"Contract deployed to {fund_me.address}")
 
 def main():
