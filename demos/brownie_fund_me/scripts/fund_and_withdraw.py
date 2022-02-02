823b09fc6201b111366b97875f771a6e6ef44347
Brian Neeland <brian.neeland@protonmail.com>
2022-02-02 11:42:58 -0700

    Added withdraw function

--- demos/brownie_fund_me/scripts/fund_and_withdraw.py
+++ demos/brownie_fund_me/scripts/fund_and_withdraw.py
@@ -9,7 +9,11 @@ def fund():
     print(f"The current entry fee is {entrance_fee}")
     fund_me.fund({"from": account, "value": entrance_fee})
 
-
+def withdraw():
+    fund_me = FundMe[-1]
+    account = get_account()
+    fund_me.withdraw({"from": account})
 
 def main():
     fund()
+    withdraw()
